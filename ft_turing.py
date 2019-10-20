a761c8eb32cc2abd123a803458ac156023c70a81
aholster <aholster@student.codam.nl>
2019-10-20 17:03:45 +0200

    redid input verification

--- ft_turing.py
+++ ft_turing.py
@@ -1,12 +1,14 @@
 import sys
 import json
 
+
 from turing_verification import verify_turing_dict
 from turing_verification import verify_turing_transition_table
 
+
 def usage():
-	'''prints usage? '''
-	print('''usage: ft_turing [-h] jsonfile input
+    '''prints usage? '''
+    print('''usage: ft_turing [-h] jsonfile input
 
 positional arguments:
 
@@ -16,33 +18,34 @@ positional arguments:
 
 optional arguments:
   -h, --help            show this help message and exit''')
-	sys.exit(1)
+    sys.exit(1)
 
 
 def verify_json_machine(json_name):
-	'''verifies json turing machine is not mangled'''
-	try:
-		with open(sys.argv[1], "r") as file:
-			turing_dict = json.load(file)
-	except:
-		print('error: could not open file')
-		sys.exit(1)
-	else:
-		if (verify_turing_dict(turing_dict) == False):
-			print('error: json machine-structure mangled')
-			sys.exit(1)
-		elif (verify_turing_transition_table(turing_dict) == False):
-			print('error: transition table invalid')
-			sys.exit(1)
+    '''verifies json turing machine is not mangled'''
+    try:
+        with open(sys.argv[1], "r") as file:
+            turing_dict = json.load(file)
+    except:
+        print('error: could not open file')
+        sys.exit(1)
+    else:
+        if (verify_turing_dict(turing_dict) == False):
+            print('error: json machine-structure mangled')
+            sys.exit(1)
+        elif (verify_turing_transition_table(turing_dict) == False):
+            print('error: transition table invalid')
+            sys.exit(1)
 
 
-if (len(sys.argv) == 2):
-	if sys.argv[1] == '-h' or sys.argv[1] == '--help':
-		usage()
-	else:
-		print('error: not enough arguments')
-		sys.exit(1)
-elif (len(sys.argv) == 3):
-	verify_json_machine(sys.argv[1])
-else:
-	usage()
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+            usage()
+        else:
+            print('error: not enough arguments')
+            sys.exit(1)
+    elif len(sys.argv) == 3:
+        verify_json_machine(sys.argv[1])
+    else:
+        print('error: too many arguments')
--- turing_verification.py
+++ turing_verification.py
@@ -1,72 +1,75 @@
 def verify_turing_transition_table(ttbl):
-	''' verifies transition table validity as defined in subject'''
-	veracity = True
-	for states in ttbl['states']:
-		if not states in ttbl['transitions'] and not states in ttbl['finals']:
-			print('Warning: Leftover State found:', states)
-			veracity = False
+    ''' verifies transition table validity as defined in subject'''
+    veracity = True
+    for states in ttbl['states']:
+        if not states in ttbl['transitions'] and not states in ttbl['finals']:
+            print('Warning: Leftover State found:', states)
+            veracity = False
 
-	ttransition_table = ttbl['transitions']
-	for state_handler in ttransition_table:
-		linecount = 0
-		if not state_handler in ttbl['states']:
-			print('Warning: Unlisted State Found: ', state_handler)
-			veracity = False
-		for event_handler in ttransition_table[state_handler]:
-			linecount += 1
-			if not 'read' in event_handler:
-				print('Warning missing field: Read in:', state_handler, 'at line:', linecount)
-				veracity = False
-			elif not event_handler['read'] in ttbl['alphabet']:
-				print('Warning: wrong character:', event_handler['read'], 'in Read Field at', state_handler, 'line:', linecount)
-				veracity = False
-			
-			if not 'to_state' in event_handler:
-				print('Warning missing field: State in:', state_handler, 'at line:', linecount)
-				veracity = False
-			elif not event_handler['to_state'] in ttbl['states']:
-				print('Warning: wrong state:', event_handler['to_state'], 'in to_state Field at', state_handler, 'line:', linecount)
-				veracity = False
-			
-			if not 'write' in event_handler:
-				print('Warning missing field: Write in:', state_handler, 'at line:', linecount)
-				veracity = False
-			elif not event_handler['write'] in ttbl['alphabet']:
-				print('Warning: wrong character:', event_handler['write'], 'in Write Field at', state_handler, 'line:', linecount)
-				veracity = False
-			
-			val_actions ='LEFT', 'RIGHT'
-			if not 'action' in event_handler:
-				print('Warning missing field: Action in:', state_handler, 'at line:', linecount)
-				veracity = False
-			elif not event_handler['action'] in val_actions:
-				print('Warning: wrong action listed at:', state_handler, 'at line', linecount)
-				veracity = False	
-	return (veracity)
+    ttransition_table = ttbl['transitions']
+    for state_handler in ttransition_table:
+        linecount = 0
+        if not state_handler in ttbl['states']:
+            print('Warning: Unlisted State Found: ', state_handler)
+            veracity = False
+        for t_instructions in ttransition_table[state_handler]:
+            linecount += 1
+            if len(t_instructions) != 4:
+                print('Warning: wrong number of fields in', f'"{state_handler}"', 'at line', linecount)
+                veracity = False
+            def print_field_error(err_str, bad_entry):
+                nonlocal veracity
+                print(err_str, f'"{bad_entry}"', 'in', f'"{state_handler}"', 'at line', linecount)
+                veracity = False
+
+            if not 'read' in t_instructions:
+                print_field_error('Warning: missing field:', 'read')
+            elif not t_instructions['read'] in ttbl['alphabet']:
+                print_field_error('Warning: wrong character entry:', t_instructions['read'])
+
+            if not 'to_state' in t_instructions:
+                print_field_error('Warning: missing field:', 'to_state')
+            elif not t_instructions['to_state'] in ttbl['states']:
+                print_field_error('Warning: wrong state entry:', t_instructions['to_state'])
+
+            if not 'write' in t_instructions:
+                print_field_error('Warning: missing field:', 'write')
+            elif not t_instructions['write'] in ttbl['alphabet']:
+                print_field_error('Warning: wrong character entry:', t_instructions['write'])
+
+            val_actions = 'LEFT', 'RIGHT'
+            if not 'action' in t_instructions:
+                print_field_error('Warning: missing field:', 'action')
+            elif not t_instructions['action'] in val_actions:
+                print_field_error('Warning: wrong action entry:', t_instructions['action'])
+    return (veracity)
 
 
 def verify_turing_dict(tdict):
-	''' verifies that turing_dict contains the necesary fields
-	also checks if said fields contain proper necesary items
-	as defined in subject
-	DOES NOT VERIFY TRANSITIONS '''
-	veracity = True
-	prereq_fields = 'name', 'alphabet', 'blank', 'states', 'initial', 'finals', 'transitions'
-	for key in prereq_fields:
-		if not key in tdict:
-			print('Field missing:', key)
-			veracity = False
-	for token in tdict['alphabet']:
-		if len(token) != 1:
-			print('Alphabet Token Invalid:', token)
-			veracity = False
-	if len(tdict['blank']) != 1:
-		print('Blank Invalid:', tdict['blank'])
-		veracity = False
-	if not tdict['blank'] in tdict['alphabet']:
-		print('Blank not defined in alphabet:', tdict['blank'])
-		veracity = False
-	if not tdict['initial'] in tdict['states']:
-		print('Initial state invalid:', tdict['initial'])
-		veracity = False
-	return (veracity)
+    ''' verifies that turing_dict contains the necesary fields
+    also checks if said fields contain proper necesary items
+    as defined in subject
+    DOES NOT VERIFY TRANSITIONS '''
+    veracity = True
+    if (len(tdict) != 7):
+        print('Warning: numer of fields should be 7 but is:', len(tdict))
+        veracity = False
+    prereq_fields = 'name', 'alphabet', 'blank', 'states', 'initial', 'finals', 'transitions'
+    for key in prereq_fields:
+        if not key in tdict:
+            print('Warning: Field missing:', key)
+            veracity = False
+    for token in tdict['alphabet']:
+        if len(token) != 1:
+            print('Warning: Alphabet Token Invalid:', token)
+            veracity = False
+    if len(tdict['blank']) != 1:
+        print('Warning: Blank Invalid:', tdict['blank'])
+        veracity = False
+    if not tdict['blank'] in tdict['alphabet']:
+        print('Warning: Blank not defined in alphabet:', tdict['blank'])
+        veracity = False
+    if not tdict['initial'] in tdict['states']:
+        print('Warning: Initial state invalid:', tdict['initial'])
+        veracity = False
+    return (veracity)
